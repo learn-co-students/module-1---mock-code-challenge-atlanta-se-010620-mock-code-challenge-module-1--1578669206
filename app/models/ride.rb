da009f86c66205c8c536a3d5ce46dd009bb93cd7
github username <github email address>
2020-01-10 11:09:55 -0500

    Wheew

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,20 @@
+class Ride
+    attr_reader :driver, :passenger, :distance
+    @@all = []
 
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance.to_f
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.average_distance
+        (Ride.all.map{ |ride| ride.distance}.sum)/Ride.all.size
+    end
+
+end
