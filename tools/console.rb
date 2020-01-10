86b0f7923f0e2ff29e573bfe181ea64e03839fbc
Dino <dinokunovac@gmail.com>
2020-01-10 11:14:59 -0500

    did not crush

--- tools/console.rb
+++ tools/console.rb
@@ -7,4 +7,31 @@ end
 
 # Put your variables here~!
 
+class Passenger
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+# passenger = Passenger.new("john")
+
+class Driver
+  attr_accessor :name
+
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end
+
+dino = Driver.new("Dino")
+lyn = Driver.new("Lyn")
+
+
+
 binding.pry
