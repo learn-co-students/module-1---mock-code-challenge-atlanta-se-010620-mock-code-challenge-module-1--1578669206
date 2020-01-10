6f25e1628e48f99386e7274d364de0fd8421e30d
Alvin Coulson <aaacoulson@gmail.com>
2020-01-10 11:12:42 -0500

    mock mod1

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,25 @@
+class Ride
+    attr_accessor :driver
+    
+    def initialize
+        @driver = driver
+        @passenger = passenger
+        @distance = distance.to_f
+    end
 
+    def passenger
+        @passenger
+    end
+
+    def driver
+        @driver
+    end
+
+    def distance
+        @distance
+    end
+
+    def average_distance
+        @average_distance = @distance.reduce {|distance| (passenger.total_distance / passenger.rides.count)}
+    end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,10 +1,13 @@
 require_relative '../config/environment.rb'
+require_relative '../passenger.rb'
+require_relative '../ride.rb'
+require_relative '../driver.rb'
 
 def reload
   load 'config/environment.rb'
 end
 
-
 # Put your variables here~!
+
 
 binding.pry
