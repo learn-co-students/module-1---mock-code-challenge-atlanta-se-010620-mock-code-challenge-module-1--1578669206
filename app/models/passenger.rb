196306145be237abc79aa85e353cca65e920660c
tylergreason <tyler.greason@protonmail.com>
2020-01-10 11:10:44 -0500

    first commit

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,38 @@
 
+class Passenger 
+    # class variables 
+    @@all = [] 
+
+    attr_accessor :name, :distances, :drivers, :rides 
+    def initialize(name)
+        @name = name 
+        @rides = [] 
+        @drivers = [] 
+        @distances = [] 
+        @@all << self 
+    end 
+
+    # instance methods 
+    def rides 
+        @rides 
+    end
+
+    def drivers 
+        @drivers 
+    end 
+    
+    def total_distance 
+        @distances.sum 
+    end 
+    
+    # class methods 
+
+    def self.all
+        @@all 
+    end
+
+    def self.premium_members 
+        # Returns an array of all Passengers who have travelled over 100 miles in total with the service
+        @@all.select{|index| index.total_distance>100}
+    end 
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,45 @@
 
+class Ride 
+    # class variables 
+    # array to track all distances 
+    @@distances = [] 
+
+
+    def initialize(driver, passenger, distance)
+        @driver = driver 
+        @passenger = passenger
+        @distance = distance
+        @@distances << distance
+        @driver.distances << distance
+        @passenger.distances << distance
+        @passenger.drivers << driver 
+        @driver.passengers << passenger
+        @driver.rides << self 
+        @passenger.rides << self 
+
+    end 
+
+    # instance methods 
+    def passenger 
+        @passenger
+    end
+
+    def driver 
+        @driver
+    end
+
+    def distance
+        @distance
+    end
+
+    
+    # class methods 
+    def self.average_distance
+        (@@distances.sum/@@distances.length).to_f
+    end
+
+    # making a method for returning @@distances for debugging 
+    def self.distances 
+        @@distances
+    end
+end 
